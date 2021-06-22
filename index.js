024c115e6e8437310f94adf3166a243e3b792e2c
yaroslav <85133193+martimine26@users.noreply.github.com>
2021-06-22 13:18:38 +0300

    newL

---
+++ index.js
@@ -0,0 +1,6 @@
+let hide = document.querySelector('.toggle');
+let plus = document.querySelector('.plus');
+function buttonClick() {
+    hide.classList.toggle('.toggle');
+    plus.classList.toggle('.black');
+}
