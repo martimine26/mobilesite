8efafa812cfb61aa640b48c8ef55e4096a54cad0
yaroslav <85133193+martimine26@users.noreply.github.com>
2021-06-22 15:14:46 +0300

    hi

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-let hide = document.querySelector('.toggle');
-let plus = document.querySelector('.plus');
-function buttonClick() {
-    hide.classList.toggle('.hide');
-    plus.classList.toggle('.black');
+let text = document.querySelector('.toggle')
+let plus = document.querySelector('.plus')
+function buttonClick(){
+    text.classList.toggle('hide')
+    plus.classList.toggle('black')
+}
+let plus1 = document.querySelector('.new')
+let text1 = document.querySelector('.toggle1')
+function buttoClick(){
+    text1.classList.toggle('hide')
+    plus1.classList.toggle('black')
 }
