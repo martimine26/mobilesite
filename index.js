0711304562fcaa2cf031fc624d7875fb92a8c5e1
yaroslav <85133193+martimine26@users.noreply.github.com>
2021-06-22 13:57:52 +0300

    vkktor

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let hide = document.querySelector('.toggle');
 let plus = document.querySelector('.plus');
 function buttonClick() {
-    hide.classList.toggle('.toggle');
+    hide.classList.toggle('.hide');
     plus.classList.toggle('.black');
 }
