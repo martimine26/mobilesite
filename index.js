eb21c54b28793097f88c2ac93e83a170110a2921
yaroslav <85133193+martimine26@users.noreply.github.com>
2021-06-25 05:01:55 +0300

    new

--- index.js
+++ index.js
@@ -10,3 +10,79 @@ function buttoClick(){
     text1.classList.toggle('hide')
     plus1.classList.toggle('black')
 }
+let org = document.querySelector('.org')
+let plus2 = document.querySelector('.new1')
+let text2 = document.querySelector('.toggle2')
+function buttClick(){
+    text2.classList.toggle('hide')
+    org.classList.toggle('background')
+    plus2.classList.toggle('black')
+}
+let org1 = document.querySelector('.org1')
+let plus3 = document.querySelector('.new2')
+let text3 = document.querySelector('.toggle3')
+function butClick(){
+    text3.classList.toggle('hide')
+    org1.classList.toggle('background')
+    plus3.classList.toggle('black')
+}
+let org2 = document.querySelector('.org2')
+let plus4 = document.querySelector('.new3')
+let text4 = document.querySelector('.toggle4')
+function buClick(){
+    text4.classList.toggle('hide')
+    org2.classList.toggle('background')
+    plus4.classList.toggle('black')
+}
+let org3 = document.querySelector('.org3')
+let plus5 = document.querySelector('.new4')
+let text5 = document.querySelector('.toggle5')
+function bClick(){
+    text5.classList.toggle('hide')
+    org3.classList.toggle('background')
+    plus5.classList.toggle('black')
+}
+let org4 = document.querySelector('.org4')
+let plus6 = document.querySelector('.new5')
+let text6 = document.querySelector('.toggle6')
+function Click(){
+    text6.classList.toggle('hide')
+    org4.classList.toggle('background')
+    plus6.classList.toggle('black')
+}
+let plus7 = document.querySelector('.new7')
+let text7 = document.querySelector('.toggle8')
+function targetClick(){
+    text7.classList.toggle('hide')
+    plus7.classList.toggle('black')
+}
+let plus8 = document.querySelector('.new8')
+let text8 = document.querySelector('.toggle9')
+function targeClick(){
+    text8.classList.toggle('hide')
+    plus8.classList.toggle('black')
+}
+let plus9 = document.querySelector('.new9')
+let text9 = document.querySelector('.toggle10')
+function targClick(){
+    text9.classList.toggle('hide')
+    plus9.classList.toggle('black')
+}
+let plus10 = document.querySelector('.new10')
+let text10 = document.querySelector('.toggle11')
+function tarClick(){
+    text10.classList.toggle('hide')
+    plus10.classList.toggle('black')
+}
+let plus11 = document.querySelector('.new11')
+let text11 = document.querySelector('.toggle12')
+function taClick(){
+    text11.classList.toggle('hide')
+    plus11.classList.toggle('black')
+}
+let plus12 = document.querySelector('.new12')
+let text12 = document.querySelector('.toggle13')
+function tClick(){
+    text12.classList.toggle('hide')
+    plus12.classList.toggle('black')
+}
