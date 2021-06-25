b1c594154259956eff4e6f4b38f7b57bcf2f98af
yaroslav <85133193+martimine26@users.noreply.github.com>
2021-06-25 12:58:34 +0300

    hi

--- index.js
+++ index.js
@@ -86,3 +86,105 @@ function tClick(){
     text12.classList.toggle('hide')
     plus12.classList.toggle('black')
 }
+let plus13 = document.querySelector('.new13')
+let text13 = document.querySelector('.toggle14')
+function createSitesClick(){
+    text13.classList.toggle('hide')
+    plus13.classList.toggle('black')
+}
+let plus14 = document.querySelector('.new14')
+let text14 = document.querySelector('.toggle15')
+function createSiteClick(){
+    text14.classList.toggle('hide')
+    plus14.classList.toggle('black')
+}
+let plus15 = document.querySelector('.new15')
+let text15 = document.querySelector('.toggle16')
+function createSitClick(){
+    text15.classList.toggle('hide')
+    plus15.classList.toggle('black')
+}
+let plus16 = document.querySelector('.new16')
+let text16 = document.querySelector('.toggle17')
+function createSiClick(){
+    text16.classList.toggle('hide')
+    plus16.classList.toggle('black')
+}
+let plus17 = document.querySelector('.new17')
+let text17 = document.querySelector('.toggle18')
+function createSClick(){
+    text17.classList.toggle('hide')
+    plus17.classList.toggle('black')
+}
+let plus18 = document.querySelector('.new18')
+let text18 = document.querySelector('.toggle19')
+function createClick(){
+    text18.classList.toggle('hide')
+    plus18.classList.toggle('black')
+}
+let plus19 = document.querySelector('.new19')
+let text19 = document.querySelector('.toggle20')
+function creatClick(){
+    text19.classList.toggle('hide')
+    plus19.classList.toggle('black')
+}
+let plus20 = document.querySelector('.new20')
+let text20 = document.querySelector('.toggle21')
+function creaClick(){
+    text20.classList.toggle('hide')
+    plus20.classList.toggle('black')
+}
+let plus21 = document.querySelector('.new21')
+let text21 = document.querySelector('.toggle22')
+function functionCrmClick(){
+    text21.classList.toggle('hide')
+    plus21.classList.toggle('black')
+}
+let plus22 = document.querySelector('.new22')
+let text22 = document.querySelector('.toggle23')
+function functionCrClick(){
+    text22.classList.toggle('hide')
+    plus22.classList.toggle('black')
+}
+let plus23 = document.querySelector('.new23')
+let text23 = document.querySelector('.toggle24')
+function functionCClick(){
+    text23.classList.toggle('hide')
+    plus23.classList.toggle('black')
+}
+let plus24 = document.querySelector('.new24')
+let text24 = document.querySelector('.toggle25')
+function functionClick(){
+    text24.classList.toggle('hide')
+    plus24.classList.toggle('black')
+}
+let plus25 = document.querySelector('.new25')
+let text25 = document.querySelector('.toggle26')
+function functioClick(){
+    text25.classList.toggle('hide')
+    plus25.classList.toggle('black')
+}
+let plus26 = document.querySelector('.new26')
+let text26 = document.querySelector('.toggle27')
+function bottomClick(){
+    text26.classList.toggle('hide')
+    plus26.classList.toggle('black')
+}
+let plus27 = document.querySelector('.new27')
+let text27 = document.querySelector('.toggle28')
+function bottoClick(){
+    text27.classList.toggle('hide')
+    plus27.classList.toggle('black')
+}
+let plus28 = document.querySelector('.new28')
+let text28 = document.querySelector('.toggle29')
+function bottClick(){
+    text28.classList.toggle('hide')
+    plus28.classList.toggle('black')
+}
+let plus29 = document.querySelector('.new29')
+let text29 = document.querySelector('.toggle30')
+function botClick(){
+    text29.classList.toggle('hide')
+    plus29.classList.toggle('black')
+}
